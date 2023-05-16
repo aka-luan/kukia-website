092622bf81d11aa6b9c445a76a0e427385d87523
Luan Alves <akaluan.la@gmail.com>
2023-05-15 21:13:16 -0300

    cool footer

--- resources/views/components/footer.php
+++ resources/views/components/footer.php
@@ -1,3 +1,43 @@
 <footer>
-  Footer
+  <div class="container footer__content">
+    <div class="row">
+      <div class="col-md-4 footer__list">
+        <h4>Services</h4>
+        <ul >
+          <li>Wedding</li>
+          <li>Personal</li>
+          <li>Enterprise</li>
+          <li>Brand</li>
+        </ul>
+      </div>
+      <div class="col-md-4 footer__list">
+        <h4>Connect</h4>
+        <ul>
+          <li>Instagram</li>
+          <li>Facebook</li>
+          <li>Twitter</li>
+          <li>Linkedin</li>
+        </ul>
+      </div>
+      <div class="col-md-4 footer__list">
+        <h4>Contact</h4>
+        <ul >
+          <li><i class="fab fa-whatsapp fa-lg"></i>(91) 99999-9999</li>
+          <li><i class="fas fa-phone"></i>(91) 99999-9999</li>
+          <li><i class="far fa-envelope"></i>contact@kukia.com.br</li>
+        </ul>
+      </div>
+    </div>
+    <div class="footer__img">
+      <div>
+        <span>P</span>
+        <span>H</span>
+        <span>O</span>
+        <span>T</span>
+        <span>O</span>
+        <span>.</span>
+      </div>
+      <img src="<?php echo get_template_directory_uri() ?>/resources/assets/footer_img.jpg" alt="" />
+    </div>
+  </div>
 </footer><!-- footer -->
